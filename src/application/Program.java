1100b5fa94ca0fb50d249934301522f24847f9ad
Douglas Phelipe <douglasphelipedpss@gmail.com>
2026-06-10 00:42:59 -0300

    department insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,22 +34,22 @@ public class Program {
             System.out.println(obj);
         }
 
-        System.out.println("===== TEST 4: seller update =====");
+        System.out.println("===== TEST 4: seller insert =====");
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
 
-        //System.out.println("===== TEST 5: seller update =====");
-        //seller = sellerDao.findById(1);
-        //seller.setName("Martha Waine");
-        //sellerDao.update(seller);
-        //System.out.println("Update completed");
+        System.out.println("===== TEST 5: seller update =====");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
 
-        //System.out.println("===== TEST 6: seller delete =====");
-        //System.out.println("Enter id for delete test: ");
-        //int id = sc.nextInt();
-        //sellerDao.deleteById(id);
-        //System.out.println("Delete completed");
+        System.out.println("===== TEST 6: seller delete =====");
+        System.out.println("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed");
 
         System.out.println("===== TEST 7: department findAll =====");
         List<Department> listDep = departmentDao.findAll();
@@ -72,5 +72,10 @@ public class Program {
         department.setName("Music");
         departmentDao.update(department);
         System.out.println("Update completed");
+
+        System.out.println("===== TEST 4: department insert =====");
+        Department newDepartment = new Department(null, "Engineering");
+        departmentDao.insert(newDepartment);
+        System.out.println("Inserted! New id = " + newDepartment.getId());
     }
 }
